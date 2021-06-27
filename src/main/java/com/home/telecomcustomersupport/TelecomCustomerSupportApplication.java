bbab49cc25b3b2dfa821a1f7a13ecc9c347f6381
M. Boufous <bfsmhmd@gmail.com>
2021-06-27 10:50:30 +0200

    init commit.

---
+++ src/main/java/com/home/telecomcustomersupport/TelecomCustomerSupportApplication.java
@@ -0,0 +1,13 @@
+package com.home.telecomcustomersupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomCustomerSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomCustomerSupportApplication.class, args);
+    }
+
+}
